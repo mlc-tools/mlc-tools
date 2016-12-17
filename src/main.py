d9991df84a0ac3a8184c8d8646485057e2eb6b59
Volodar <tolm_vl@hotmail.com>
2016-12-17 18:32:33 +0300

    remove unused files

--- src/main.py
+++ src/main.py
@@ -1,12 +1,25 @@
+import fileutils
 from Parser import Parser
 from WriterCpp import WriterCpp
 from WriterJava import WriterJava
+
+
+def removeOldFiles(parser):
+	files = fileutils.getFilesList( "out/" )
+	for file in files:
+		name = file[0:file.find(".")]
+		if parser._findClass(name) == None:
+			print "remove", "out/" + file
+			fileutils.remove("out/" + file)
+	return
+	
 
 str = open("declaration.txt","r").read()
 
 parser = Parser()
 parser.parse(str)
 writer = WriterCpp("out", parser)
+removeOldFiles(parser)
 
 #parser = Parser()
 #parser.parse(str)
