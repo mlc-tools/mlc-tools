6e386a57a0e2f0fb7bb1a6b86d76acbf7dfbfa3f
Volodar <tolm_vl@hotmail.com>
2016-12-17 15:33:19 +0300

    test visitor

--- tests/cpp/Test/main.cpp
+++ tests/cpp/Test/main.cpp
@@ -1,10 +1,24 @@
-#include "../../../out/Request.h"
+#include "../../../out/RequestScore.h"
 #include "../../../out/SerializedObject.h"
+#include "../../../out/IVisitorRequest.h"
 
 using namespace mg;
 
+class Visitor : public IVisitorRequest
+{
+	virtual void visit( Request* ctx )
+	{
+	}
+
+	virtual void visit( RequestScore* ctx )
+	{
+	}
+};
+
 int main()
 {
-	Request request;
+	Visitor visitor;
+	Request* request = new RequestScore;
+	request->accept( &visitor );
 	return 0;
 }
