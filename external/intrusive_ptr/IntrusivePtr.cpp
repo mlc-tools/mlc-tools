ab40b3a5b457e876e0f625414c5915d9f46f10ea
v.tolmachev <v.tolmachev@rocketjump.ru>
2017-04-18 15:26:46 +0300

    added intrusive_ptr

---
+++ external/intrusive_ptr/IntrusivePtr.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************/
+/*
+ * Copyright 2014-2015 Vladimir Tolmachev
+ *
+ * Author: Vladimir Tolmachev
+ * Project: ml
+ * e-mail: tolm_vl@hotmail.com
+ * If you received the code is not the author, please contact me
+ */
+/******************************************************************************/
+
+#include "IntrusivePtr.h"
+#include "SerializedObject.h"
+
+void __IntrusivePtr__retain( mg::SerializedObject* ptr )
+{
+	ptr->retain();
+}
+
+void __IntrusivePtr__release( mg::SerializedObject* ptr )
+{
+	ptr->release();
+}
+
