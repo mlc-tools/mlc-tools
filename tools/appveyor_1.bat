5f224b6168a53432cecdc23b4ad7fd49df9340bd
VladimirTolmachev(Home OSX) <tolm_vl@hotmail.com>
2017-05-01 11:07:02 +0300

    add appveyor build script

--- tools/appveyor_1.bat
+++ tools/appveyor_1.bat
@@ -1,7 +1,7 @@
-python ../src/main.py -i ../simple_test/config -o ../simple_test/generated/xml -f xml -l cpp -side server
-cd ../simple_test
+python src/main.py -i simple_test/config -o simple_test/generated/xml -f xml -l cpp -side server
+cd simple_test
 mkdir build
 cd build 
-cmake .. -DBUILD_XML=ON -DBUILD_SIDE=SERVER
+cmake .. -DBUILD_XML=ON -DBUILD_JSON=OFF -DBUILD_SIDE=SERVER
 MSBuild test_mlc_xml.sln /p:Configuration=Release /p:Platform=win32
 "Release/test_mlc_xml.exe"
--- tools/appveyor_2.bat
+++ tools/appveyor_2.bat
@@ -1,7 +1,7 @@
-python ../src/main.py -i ../simple_test/config -o ../simple_test/generated/xml -f json -l cpp -side client
-cd ../simple_test
+python src/main.py -i simple_test/config -o simple_test/generated/json -f json -l cpp -side client
+cd simple_test
 mkdir build
 cd build 
-cmake .. -DBUILD_XML=ON -DBUILD_SIDE=CLIENT
-MSBuild test_mlc_xml.sln /p:Configuration=Release /p:Platform=win32
-"Release/test_mlc_xml.exe"
+cmake .. -DBUILD_XML=OFF -DBUILD_JSON=ON -DBUILD_SIDE=CLIENT
+MSBuild test_mlc_json.sln /p:Configuration=Release /p:Platform=win32
+"Release/test_mlc_json.exe"
