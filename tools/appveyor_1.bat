ce041c4062a8c7cfb41cf2e4e1c583d05d15987b
VladimirTolmachev(Home OSX) <tolm_vl@hotmail.com>
2017-05-31 09:27:47 +0300

    removed appveyor_x.bat files

--- tools/appveyor_1.bat
+++
@@ -1,7 +0,0 @@
-python src/main.py -i simple_test/config -o simple_test/generated/xml -f xml -l cpp -side server -data simple_test/data_xml -data_out simple_test/assets
-cd simple_test
-mkdir build
-cd build 
-cmake .. -DBUILD_XML=ON -DBUILD_JSON=OFF -DBUILD_SIDE=SERVER
-MSBuild test_mlc_xml.sln /p:Configuration=Release /p:Platform=win32
-"Release/test_mlc_xml.exe"
--- tools/appveyor_2.bat
+++
@@ -1,7 +0,0 @@
-python src/main.py -i simple_test/config -o simple_test/generated/json -f json -l cpp -side client -data simple_test/data_json -data_out simple_test/assets
-cd simple_test
-mkdir build
-cd build 
-cmake .. -DBUILD_XML=OFF -DBUILD_JSON=ON -DBUILD_SIDE=CLIENT
-MSBuild test_mlc_json.sln /p:Configuration=Release /p:Platform=win32
-"Release/test_mlc_json.exe"
