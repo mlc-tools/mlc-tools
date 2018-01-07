dc81ee672fdcae5198e5d89809ecc68f8eeb17d4
VladimirTolmachev(Home OSX) <tolm_vl@hotmail.com>
2018-01-07 14:23:32 +0700

    fix tests

--- simple_test/test_cpp.py
+++ simple_test/test_cpp.py
@@ -2,5 +2,5 @@ import os
 
 if os.system('cd simple_test/build; cmake .. -DBUILD_XML=ON -DBUILD_JSON=OFF -DBUILD_SIDE=CLIENT; make -j8;') != 0:
     exit(1)
-if os.system('./simple_test/build/test_mlc_xml simple_test/') != 0:
+if os.system('./simple_test/build/test_mlc_xml ./simple_test/') != 0:
     exit(1)
