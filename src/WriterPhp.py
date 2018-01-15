90f1751ed516c01cdc7da40f3d9610676ede1bc0
Vladimir Tolmachev <tolm_vl@hotmail.com>
2018-01-15 17:18:30 +0700

    php: replace std::round -> round

--- src/WriterPhp.py
+++ src/WriterPhp.py
@@ -450,6 +450,7 @@ def convert_function_to_php(func, parser, function_args):
         ['$$', '$'],
         ['std::$max', 'max'],
         ['std::$min', 'min'],
+        ['std::$round', 'round'],
         ['in_list(', 'in_array('],
         ['in_map', 'array_key_exists'],
         ['list_push', 'array_push'],
