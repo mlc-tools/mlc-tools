2362f917a6922112b7aff0791a5fdf29987a80a9
VladimirTolmachev(Home OSX) <tolm_vl@hotmail.com>
2018-01-31 21:13:14 +0700

    fix cpp compilation

--- src/cpp_functions.py
+++ src/cpp_functions.py
@@ -6,21 +6,21 @@ hpp_functions = '''
 #include <vector>
 
 template <class T, class P>
-bool in_map(const T& element, const std::map<T, P>& map)
+bool in_map(const T& key, const std::map<T, P>& map)
 {
-    return map.count(element) > 0;
+    return map.count(key) > 0;
 }
 
-template <class T, class P>
-bool in_map(int element, const std::map<T, P>& map)
+template <class P>
+bool in_map(int key, const std::map<int, P>& map)
 {
-    return map.count(element) > 0;
+    return map.count(key) > 0;
 }
 
 template <class T>
-bool in_list(const T& element, const std::vector<T>& list)
+bool in_list(const T& item, const std::vector<T>& list)
 {
-    return std::find(list.begin(), list.end(), element) != list.end();
+    return std::find(list.begin(), list.end(), item) != list.end();
 }
 
 template <class T>
