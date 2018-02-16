d0b6368336f8d8d6707806970c0c4ecf1894dd51
Vladimir Tolmachev <tolm_vl@hotmail.com>
2018-02-16 15:41:04 +0700

    fix build test_serialize

--- test_serialize/run.py
+++ test_serialize/run.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 root = os.path.dirname(os.path.abspath(__file__)) + '/..'
 
@@ -23,26 +24,33 @@ def generate(lang, protocol):
     if 0 != os.system(command):
         exit(1)
 
+
+def clean():
+    if os.path.isfile('../test_serialize/data.py.xml'):
+        os.remove('../test_serialize/data.py.xml')
+    if os.path.isfile('../test_serialize/data.php.xml'):
+        os.remove('../test_serialize/data.php.xml')
+    if os.path.isfile('../test_serialize/data.cpp.xml'):
+        os.remove('../test_serialize/data.cpp.xml')
+    if os.path.isfile('../test_serialize/step_2'):
+        os.remove('../test_serialize/step_2')
+    if os.path.isdir('../test_serialize/build'):
+        shutil.rmtree('../test_serialize/build')
+
 generate('py', 'xml')
 generate('php', 'xml')
 generate('cpp', 'xml')
-
-if os.path.isfile('../test_serialize/data.py.xml'):
-    os.remove('../test_serialize/data.py.xml')
-if os.path.isfile('../test_serialize/data.php.xml'):
-    os.remove('../test_serialize/data.php.xml')
-if os.path.isfile('../test_serialize/data.cpp.xml'):
-    os.remove('../test_serialize/data.cpp.xml')
-if os.path.isfile('../test_serialize/step_2'):
-    os.remove('../test_serialize/step_2')
-
+clean()
 command = '''
     cd ../test_serialize;
     python step_0.py;
     php step_1.php;
-    cd build; make install; cd ..; ./step_2;
+    mkdir build; cd build; cmake ..; make -j8 install; cd ..; ./step_2;
     python step_3.py;
     '''
 command = command.replace('\n', '')
 if 0 != os.system(command):
+    clean()
     exit(1)
+
+clean()
