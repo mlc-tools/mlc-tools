feafcd157ef64be5b67b1568cf173342b12458cd
VladimirTolmachev(Home OSX) <tolm_vl@hotmail.com>
2018-05-20 11:40:22 +0700

    git hook

--- tools/hooks/pre-commit.py
+++ tools/hooks/pre-commit.py
@@ -3,5 +3,8 @@ import os
 if __name__ == '__main__':
     root = os.path.dirname(os.path.abspath(__file__)) + '/../..'
 
-    os.system('python  {}/tests/mlc_build.py'.format(root))
-    os.system('python3 {}/tests/mlc_build.py'.format(root))
+    if 0 != os.system('python  {}/tests/mlc_build.py'.format(root)):
+        exit(1)
+    if 0 != os.system('python3 {}/tests/mlc_build.py'.format(root)):
+        exit(1)
+    exit(0)
