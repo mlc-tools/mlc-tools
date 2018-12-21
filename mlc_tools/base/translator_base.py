d51ed9800a01f72a3089859334a689fc461c15b1
Vladimir Tolmachev <tolm_vl@hotmail.com>
2018-12-21 12:57:26 +0700

    refactoring: Translator

--- mlc_tools/base/translator_base.py
+++ mlc_tools/base/translator_base.py
@@ -1,3 +1,4 @@
+from ..core.object import Object, AccessSpecifier
 
 
 class TranslatorBase(object):
@@ -16,4 +17,33 @@ class TranslatorBase(object):
         pass
 
     def convert_to_enum(self, cls):
-        pass
+        shift = 0
+        cast = 'string'
+        values = []
+        for member in cls.members:
+            if member.name:
+                continue
+            member.name = member.type
+            member.type = cast
+            member.is_static = True
+            member.is_const = True
+            if member.initial_value is None:
+                if cast == 'int':
+                    member.initial_value = '(1 << {})'.format(shift)
+                    values.append(1 << shift)
+                elif cast == 'string':
+                    member.initial_value = '"{}"'.format(member.name)
+            elif cast == 'int':
+                # TODO if initialization is as enumerate of others members need throw error (example: one|two)
+                values.append(member.initial_value)
+            else:
+                member.initial_value = 'None'
+
+            shift += 1
+        value = Object()
+        value.initial_value = '{}::{}'.format(cls.name, cls.members[0].name)
+        value.name = '_value'
+        value.type = cast
+        value.access = AccessSpecifier.private
+        cls.members.append(value)
+        return values
