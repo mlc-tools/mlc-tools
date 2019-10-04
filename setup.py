772d57da5cebb98f27249ce402a19049bc86c4fd
Vladimir Tolmachev <tolm_vl@hotmail.com>
2019-10-04 16:57:43 +0700

    pip3 upload

--- setup.py
+++ setup.py
@@ -1,17 +1,40 @@
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
+# import mlc_tools.version
+
+# setup(
+#     name='mlc-tools',
+#     version=mlc_tools.version,
+#     packages=find_packages(),
+#     long_description="mlc-tools",
+#     install_requires=[
+#         'enum'
+#     ]
+#     # entry_points={
+#     #     'console_scripts':
+#     #         ['mlc-tools = mlc_tools.main:console']
+#     # },
+#     # test_suite='tests'
+# )
+import setuptools
 import mlc_tools.version
 
-setup(
-    name='mlc-tools',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mlc-tools",
     version=mlc_tools.version,
-    packages=find_packages(),
-    long_description="mlc-tools",
-    install_requires=[
-        'enum'
-    ]
-    # entry_points={
-    #     'console_scripts':
-    #         ['mlc-tools = mlc_tools.main:console']
-    # },
-    # test_suite='tests'
+    author="Vladimir Tolmachev",
+    author_email="tolm_vl@hotmail.com",
+    description="A tool to generate and translate C++ code to other languages from one code-base.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mlc-tools/mlc-tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
--- upload.py
+++ upload.py
@@ -3,10 +3,10 @@ import sys
 
 
 def main():
-    print sys.argv
+    print(sys.argv)
     job_number = sys.argv[2]
     if not job_number.endswith('1'):
-        print 'skip'
+        print('skip')
         sys.exit(0)
 
     version = open('mlc_tools/version.py').read().strip()
@@ -18,15 +18,15 @@ def main():
     version = version[version.find("'") + 1:]
     version = version[:version.find("'")]
 
-    print 'current version is', version
+    print('current version is', version)
 
-    if 0 != os.system('python setup.py sdist'):
-        print 'Error: sdist'
+    if 0 != os.system('python3 setup.py sdist bdist_wheel'):
+        print('Error: sdist')
         sys.exit(-1)
-    if 0 != os.system('twine upload dist/mlc-tools-{}.tar.gz -u $PYPI_USERNAME -p $PYPI_PASSWORD '.format(version)):
-        print 'Error: twine upload'
+    if 0 != os.system('python3 -m twine upload --repository pypi dist/mlc-tools-{}.tar.gz -u $PYPI_USERNAME -p $PYPI_PASSWORD '.format(version)):
+        print('Error: twine upload')
         sys.exit(-1)
-    print 'Done'
+    print('Done')
 
 
 if __name__ == '__main__':
