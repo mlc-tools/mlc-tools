17068bacf446ef728d338db953fda0c6d0d283ca
Vladimir Tolmachev <tolm_vl@hotmail.com>
2019-11-02 11:18:14 +0700

    Volodar patch 2 (#9)
    github actions

---
+++ tools/change_version.py
@@ -0,0 +1,9 @@
+import subprocess
+
+process = subprocess.Popen(['git', 'rev-list', 'HEAD', '--count'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+out, err = process.communicate(timeout=60)
+if err:
+    exit(1)
+version = int(out.decode('utf-8'))
+print(version)
+open('../mlc_tools/version.py', 'w').write(f'__version__="0.3.{version}"')
