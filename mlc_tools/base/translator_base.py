e597fa45feba49038cc634c4b0d25b4ee9a578f0
Vladimir Tolmachev <tolm_vl@hotmail.com>
2021-01-28 12:52:52 +0700

    Enum: correct generate enums with int values

--- mlc_tools/base/translator_base.py
+++ mlc_tools/base/translator_base.py
@@ -48,11 +48,9 @@ class TranslatorBase(object):
                     values.append(1 << shift)
                 elif cast == 'string':
                     member.initial_value = '"{}"'.format(member.name)
-            elif cast == 'int':
+            else:
                 # TODO if initialization is as enumerate of others members need throw error (example: one|two)
                 values.append(member.initial_value)
-            else:
-                member.initial_value = 'None'
 
             shift += 1
         value = Object()
